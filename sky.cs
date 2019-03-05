02090a47eacad9f1a23e71fc654c09d4d6bd323f
shadowhw <shadowhw@yeah.net>
2019-03-05 21:23:52 +0800

    第一次提交

---
+++ sky.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTU
+{
+    //天空类
+    public class sky
+    {
+        public int[] sky_block;
+        public int Num_block = 15;
+
+        public sky()
+        {
+            sky_block = new int[15]{3, 0, 3, 3, 0, 3, 0, 0, 3, 0, 3, 0, 3, 0, 3 };
+        }
+
+        public void tpye1()
+        {
+            sky_block = new int[15] { 3, 0, 3, 3, 0, 3, 0, 0, 3, 0, 3, 0, 3, 0, 3 };
+        }
+
+        public void tpye2()
+        {
+            sky_block = new int[15] { 0, 3, 0, 3, 0, 3, 0, 3, 0, 0, 3, 0, 3, 3, 0 };
+        }
+
+        public void tpye3()
+        {
+            sky_block = new int[15] { 3, 0, 0, 3, 0, 3, 0, 3, 0, 0, 3, 0, 0, 0, 3 };
+        }
+
+        public void tpye4()
+        {
+            sky_block = new int[15] { 0, 0, 3, 0, 3, 0, 3, 0, 3, 3, 3, 3, 0, 0, 0 };
+        }
+    }
+}
